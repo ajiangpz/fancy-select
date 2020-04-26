a3da0be5e694cb34fbef82391ae9215f1010bcea
pzzz <1174072083@qq.com>
2020-04-26 23:27:06 +0800

    【Add】SingleValue组件

---
+++ src/components/SingleValue.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  inject: ["instance"],
+  render() {
+    const { instance } = this;
+    if (!instance.hasValue) {
+      return null;
+    }
+    const node = instance.selectedNodes[0];
+    const customLabelRender = instance.$scopedSlots["value-label"];
+    return customLabelRender ? (
+      customLabelRender({ node })
+    ) : (
+      <div class="fancy-select__single-value">{node.label}</div>
+    );
+  },
+};
+</script>
