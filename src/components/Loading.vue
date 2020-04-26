f09ab31e5d7b245628d49f5e605b6facfc378dad
pzzz <1174072083@qq.com>
2020-04-26 23:24:26 +0800

    【Add】loading组件

---
+++ src/components/Loading.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  name: "Loading",
+  functional: true,
+  render(_, context) {
+    const { children } = context;
+    return (
+      <div class="fancy-select__loading">
+        <div class="fancy-select__icon-container">
+          <div class="donut"></div>
+        </div>
+        <div class="fancy-select__loading-text">{children}</div>
+      </div>
+    );
+  },
+};
+</script>
