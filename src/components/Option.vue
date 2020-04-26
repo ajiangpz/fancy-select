45981e2b80c76425143702cb085f553673fd70bd
pzzz <1174072083@qq.com>
2020-04-26 23:26:10 +0800

    【Update】options组件

--- src/components/Option.vue
+++ src/components/Option.vue
@@ -1,17 +1,18 @@
 <script>
 import ArrowIcon from "./icons/Arrow";
 import { UNCHECKED, CHECKED, INDETERMINATE } from "../contants";
-
+import Loading from "./Loading";
+import Tip from "./Tip";
 let arrowPlaceHolder, checkedMark, minusMark;
 const Option = {
   props: {
     node: {
       type: Object,
-      required: true
-    }
+      required: true,
+    },
   },
   inject: ["instance"],
-  name: "fancy-tree-treenode",
+  name: "fancy-select-treenode",
   computed: {
     shouldShowNode() {
       const { instance, node } = this;
@@ -20,43 +21,59 @@ const Option = {
     shouldExpand() {
       const { instance, node } = this;
       return instance.shouldExpand(node);
-    }
+    },
+  },
+  components: {
+    Loading,
+    Tip,
   },
   methods: {
     renderParentNode() {
-      const { instance, node } = this;
+      const { node } = this;
       const nodeClass = {
-        "fancy-tree__treenode-constainer": true,
-        // "fancy-tree__treenode--selected": instance.isSelected(node),
-        "fancy-tree__treenode--disabled": node.isDisabled,
-        "fancy-tree__treenode--matched": node.isMatched,
-        [`fancy-tree__indent-level-${node.level}`]: true,
-        "fancy-tree__treenode--hide": !this.shouldShowNode
+        "fancy-select__treenode-constainer": true,
+        // "fancy-select__treenode--selected": instance.isSelected(node),
+        "fancy-select__treenode--disabled": node.isDisabled,
+        "fancy-select__treenode--matched": node.isMatched,
+
+        "fancy-select__treenode--hide": !this.shouldShowNode,
       };
 
       return (
         <div class={nodeClass} data-id={node.id}>
           {this.renderArrow()}
           <div
-            class="fanct-tree__label-container"
+            class="fancy-select__label-container"
             onmousedown={this.handleNodeClick}
           >
             {this.renderCheckBox()}
-            <label class="fancy-tree__label">{node[instance.label]}</label>
+            {this.renderLabel()}
           </div>
         </div>
       );
+    },
+    renderLabel() {
+      const { instance, node } = this;
+      const customLabelRenderer = instance.$scopedSlots["option-label"];
+      const labelClassName = "fancy-select__label";
+      if (customLabelRenderer) {
+        return customLabelRenderer({
+          node,
+          labelClassName,
+        });
+      }
+      return <label class={labelClassName}>{node.label}</label>;
     },
     renderArrow() {
       const { instance, node } = this;
       if (node.isParent) {
         const arrowClass = {
-          "fancy-tree__treenode-arrow": true,
-          "fancy-tree__treenode-arrow--rotated": this.shouldExpand  
+          "fancy-select__treenode-arrow": true,
+          "fancy-select__treenode-arrow--rotated": this.shouldExpand,
         };
         return (
           <div
-            class="fancy-tree__treenode-arrow-container"
+            class="fancy-select__treenode-arrow-container"
             onmousedown={this.handleArrowClick}
           >
             <ArrowIcon class={arrowClass} />
@@ -66,7 +83,7 @@ const Option = {
       if (instance.hasParentNodes) {
         if (!arrowPlaceHolder) {
           arrowPlaceHolder = (
-            <div class="fancy-tree__treenode-arrow-placeholder">&nbsp;</div>
+            <div class="fancy-select__treenode-arrow-placeholder">&nbsp;</div>
           );
         }
         return arrowPlaceHolder;
@@ -74,12 +91,29 @@ const Option = {
 
       return null;
     },
+
+    renderNoChildrenTip() {
+      const { instance, node } = this;
+
+      if (!node.childrenStates.isLoaded || node.children.length) return null;
+
+      return (
+        <Tip type="no-children" icon="warning">
+          {instance.noChildrenText}
+        </Tip>
+      );
+    },
     handleArrowClick() {
+      let nextState;
       const { instance, node } = this;
       if (instance.localSearch.active) {
-        node.isExpandedOnSearch = !node.isExpandedOnSearch;
+        nextState = node.isExpandedOnSearch = !node.isExpandedOnSearch;
       } else {
-        node.isExpanded = !node.isExpanded;
+        nextState = node.isExpanded = !node.isExpanded;
+      }
+      if (nextState && !node.childrenStates.isLoaded) {
+        // 传入原始的数据
+        instance.loadChildren(node.raw);
       }
     },
     handleNodeClick() {
@@ -88,22 +122,25 @@ const Option = {
     },
     renderCheckBox() {
       const { instance, node } = this;
+      if (instance.single) {
+        return null;
+      }
       const checkedState = instance.forest.checkStateMap[node.id];
       const checkClass = {
-        "fancy-tree__checkbox": true,
-        "fancy-tree__checkbox--checked": checkedState == CHECKED,
-        "fancy-tree__checkbox--unchecked": checkedState == UNCHECKED,
-        "fancy-tree__checkbox--indeterminate": checkedState === INDETERMINATE,
-        "fancy-tree__checkbox--disabled": node.isDisabled
+        "fancy-select__checkbox": true,
+        "fancy-select__checkbox--checked": checkedState == CHECKED,
+        "fancy-select__checkbox--unchecked": checkedState == UNCHECKED,
+        "fancy-select__checkbox--indeterminate": checkedState === INDETERMINATE,
+        "fancy-select__checkbox--disabled": node.isDisabled,
       };
       if (!checkedMark) {
-        checkedMark = <span class="fancy-tree__check-mark"></span>;
+        checkedMark = <span class="fancy-select__check-mark"></span>;
       }
       if (!minusMark) {
-        minusMark = <span class="fancy-tree__minus-mark"></span>;
+        minusMark = <span class="fancy-select__minus-mark"></span>;
       }
       return (
-        <div class="fancy-tree__checkbox-container">
+        <div class="fancy-select__checkbox-container">
           <span class={checkClass}>
             {checkedMark}
             {minusMark}
@@ -113,30 +150,42 @@ const Option = {
     },
     renderChildrenNodes() {
       const { instance, node } = this;
-      if(!this.shouldExpand){
+      if (!this.shouldExpand) {
         return null;
       }
-      return node[instance.childKey].map(childNode => (
+      if (!node.childrenStates.isLoaded) {
+        return null;
+      }
+
+      return node[instance.childKey].map((childNode) => (
         <Option node={childNode} key={childNode.id} />
       ));
     },
     renderChildrenNodesList() {
-      const { instance, node } = this;
-      if (!node[instance.childKey].length) {
-        return null;
-      }
+      if (!this.shouldExpand) return null;
       return (
-        <div class="fancy-tree__children-content">
+        <div class="fancy-select__children-content">
+          {this.renderNoChildrenTip()}
+          {this.renderLoadingChildrenTip()}
           {this.renderChildrenNodes()}
         </div>
       );
-    }
+    },
+    renderLoadingChildrenTip() {
+      const { node, instance } = this;
+      if (!node.childrenStates.isLoading) {
+        return null;
+      }
+      return <loading>{instance.loadingText}</loading>;
+    },
   },
 
   render() {
     const { node } = this;
+    const indentLevel = node.level;
     const listItemClass = {
-      "fancy-tree__list-item": true
+      "fancy-select__list-item": true,
+      [`fancy-select__indent-level-${indentLevel}`]: true,
     };
     return (
       <div class={listItemClass}>
@@ -144,7 +193,7 @@ const Option = {
         {node.isParent && this.renderChildrenNodesList()}
       </div>
     );
-  }
+  },
 };
 export default Option;
 </script>
